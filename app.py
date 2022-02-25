2de769689d2bdf8ca13124cc0df9ff89618423c6
PabloApser <pablo.martinez@apser.es>
2022-02-25 13:21:48 +0100

    Creada clase db que alojará los métodos de la bse de datos de SQLALchemy, instanciado en el app.py que tiene la configuración de la app

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 # Importar el framework
 from flask import Flask
 
+# Se crea para instanciar la bbdd
+from flask_sqlalchemy import SQLAlchemy
 
 # Configuración básica para el servidor, inicializar en la variable app. Que es en sí la app servidor
 app = Flask(__name__)
+
+# Para pasarle la configuración que necesitará la base
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/concesionario.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Recibe la app, y se instnacia como una bd
+db = SQLAlchemy(app)
---
+++ utils/db.py
@@ -0,0 +1,5 @@
+# Del módulo importar la clase de SQL Alchemy
+from flask_sqlalchemy import SQLAlchemy
+
+#Se ejecuta y devuelve una instancia de db, pero se configurará en app.py
+db = SQLAlchemy()
