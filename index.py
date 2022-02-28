8fd300d32f2cce658fa43012093181b11996d967
PabloApser <pablo.martinez@apser.es>
2022-02-28 11:11:30 +0100

    referencia para que se cree la bbdd al ejecutar index.py

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 from app import app
+from utils.db import db
 
+# cuando arranque la app creará el modelo definido
+with app.app_context():
+    db.create_all()
 
 
 # Condición para verififcar si se ejecuta como proyecto principal
