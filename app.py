a0642840bcecea38396fecc22ed653e13c0ee157
PabloApser <pablo.martinez@apser.es>
2022-03-15 16:03:19 +0100

    maquetado codigo y comentarios htmls

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # Importar el framework
 from flask import Flask, render_template
-
 # Se crea para instanciar la bbdd
 from flask_sqlalchemy import SQLAlchemy
-
+# Se importan los service
 from routes.serie import series
 from routes.marca import marcas
 from routes.vehiculo import vehiculos
@@ -11,22 +10,24 @@ from routes.vehiculo import vehiculos
 # Configuración básica para el servidor, inicializar en la variable app. Que es en sí la app servidor
 app = Flask(__name__)
 
-app.secret_key = 'secret key'
 # Para pasarle la configuración que necesitará la base
+app.secret_key = 'secret key'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/concesionario.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# Recibe la app, y se instnacia como una bd
+# Recibe la app, y se instancia como una bd
 db = SQLAlchemy(app)
 
+# Función para lanzar el template de error 404 en caso de suceder en el servidor
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
+# Se registra en el objeto de la configuración los blueprints creados en los services en routes/
 app.register_blueprint(series)
 app.register_blueprint(marcas)
 app.register_blueprint(vehiculos)
-
+# Función para registrar la página de error
 app.register_error_handler(404, page_not_found)
 
--- index.py
+++ index.py
@@ -1,4 +1,6 @@
+# Se importa el objeto app de la clase creada con la configuración
 from app import app
+# Se importa el objeto con la base de datos
 from utils.db import db
 import os
 
@@ -6,9 +8,10 @@ import os
 with app.app_context():
     db.create_all()
 
-
 # Condición para verififcar si se ejecuta como proyecto principal
 if __name__ == "__main__":
+    # DESCOMENTAR para cambiar el puerto y host por defecto
     # port = int(os.environ.get('PORT', 5000))
     # app.run(debug=True, host='0.0.0.0', port=port)
+    # Arranca la aplicación
     app.run(debug=True)
